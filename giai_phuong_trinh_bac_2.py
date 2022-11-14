fbb4983658ce864872f9955f952a4b585a6046ef
Nguyễn Uy <117989566+imnguy06@users.noreply.github.com>
2022-11-14 20:10:22 +0700

    Update giai_phuong_trinh_bac_2.py

--- giai_phuong_trinh_bac_2.py
+++ giai_phuong_trinh_bac_2.py
@@ -6,8 +6,10 @@ c = float(input())
 d = b**2 - 4*a*c
 
 if d < 0:
-    print("PTVN")
+    print('PTVN')
+elif d == 0:
+    print(int(-b / (2 * a)))
+    print(int(-b / (2 * a)))
 else:
-    x1 = (-b - sqrt(d))/(2*a)
-    x2 = -b/a - x1
-    print(int(x2), int(x1))
+    print(int((-b - sqrt(d))/(2 * a)))
+    print(int((-b + sqrt(d))/(2 * a)))
